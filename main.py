5a80150c59edba21003b7ad7c5619acba43628ab
James Frost <git@frost.cx>
2021-10-05 16:31:18 +0100

    finish up for the day.

--- main.py
+++ main.py
@@ -2,29 +2,28 @@
 import time
 
 
-
 # Constants
-moon_g = -1.625 # Acceleration due to gravity on the moon.
-
+moon_g = -1.625  # Acceleration due to gravity on the moon.
 
 
 class Lander:
     def __init__(self):
-        self.fuel = 100.0 # kg of fuel
-        self.mass = 1000.0 # Mass of lander in kg
-        self.x = 0.0 # x position in metres
+        self.fuel = 100.0  # kg of fuel
+        self.mass = 1000.0  # Mass of lander in kg
+        self.x = 0.0  # x position in metres
         self.vx = 0.0
-        self.y = 0.0 # y position in metres
+        self.y = 0.0  # y position in metres
         self.vx = 0.0
-        self.z = 100.0 # height ao
+        self.z = 100.0  # height above the moon.
         self.vz = 0.0
+        self.Fz = 0.0  # Thrust of engine.
 
-    
     def total_mass(self):
         return self.mass + self.fuel
 
 
 ticktime = time.monotonic
+
 
 def time_since_last_tick():
     global ticktime
@@ -36,13 +35,14 @@ def time_since_last_tick():
 
 def physics_tick():
     dt = time_since_last_tick()
-    vz = lander.vz
-    
-    
+    global moon_g
+
+    az = lander.Fz / lander.total_mass() + moon_g
+    lander.vz = lander.vz + az * dt
+    lander.z = lander.z + lander.vz * dt
 
 
 lander = Lander
-
 
 
 if __name__ == "__main__":
